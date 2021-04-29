82ba6a3d14e8bcd81e9b0eace3ad87e0ef51a6b1
tobil <tobilesi@gmail.com>
2021-04-29 02:01:07 +0100

    created the review model including a score and review text

---
+++ project/reviews/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Review(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    text = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    score = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name} review on SilverBullet - {self.text}- With the Rating - {self.score}"
