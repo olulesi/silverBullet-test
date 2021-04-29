b5408e8f770551067c9d0d516c85409f488158db
tobil <tobilesi@gmail.com>
2021-04-29 19:22:53 +0100

    score property is now in the margins of 1 to 5

--- reviews/models.py
+++ reviews/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
+    RATINGS = (
+        (1, '1 - Terrible'),
+        (2, '2 - Poor'),
+        (3, '3 - Average'),
+        (4, '4 - Good'),
+        (5, '5 - High'),
+      )
+
     name = models.CharField(max_length=30, unique=True)
     comment = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
-    score = models.PositiveIntegerField()
+    score = models.PositiveIntegerField(choices=RATINGS)
 
     def __str__(self):
         return f"{self.name}'s review on SilverBullet - {self.text} - With the Rating - {self.score}"
