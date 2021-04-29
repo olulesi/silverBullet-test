1679e3b3ddaf77f373e1ac8c5aea7f892e97bd62
tobil <tobilesi@gmail.com>
2021-04-29 13:41:30 +0100

    created review serializer

--- reviews/models.py
+++ reviews/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Review(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    text = models.TextField(max_length=300)
+    comment = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     score = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.name} review on SilverBullet - {self.text}- With the Rating - {self.score}"
+        return f"{self.name}'s review on SilverBullet - {self.text} - With the Rating - {self.score}"
